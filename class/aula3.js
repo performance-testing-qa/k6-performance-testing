3475b439a69bb922faf3ef1f24f2fe4ca5c7fb2a
julianabaicar <baicarjuliana@gmail.com>
2025-07-20 08:33:58 -0300

    fix: ./class/aula3.js ajust in module call.

--- class/aula3.js
+++ class/aula3.js
@@ -8,7 +8,9 @@ import http from 'k6/http';
 // Nota1
     // primeiro: importar o modulo do k6 que realiza esse tipo de validacao 
     // https://grafana.com/docs/k6/latest/using-k6/modules/
-import check from 'k6';
+    // obs: quando for importar o modulo, deve-se colocar o nome do modulo
+    // entre colchetes, como abaixo.
+import { check } from 'k6';
 
 export const options = {
     vus: 1,
@@ -16,14 +18,15 @@ export const options = {
 }
 
 export default function(){
-
+    
+// Nota2
     // segundo: armazenar o conteudo que e retornado da requisicao 
     // em algum lugar. Abaixo colocamos em uma contante.
     const rest = http.get('http://test.k6.io');
 
     // terceiro: utilizamos o modulo check para validar o objeto.
     check(rest,{
-        'status coide é 200': (r) => r.status === 200
-        
+
+        'status code é 200': (r) => r.status === 200
     })
 }
