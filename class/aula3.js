a90836669b69756d4b9d881d5215e15261e0f59d
Juliana Baiçar Silva <111237997+julianabaicar@users.noreply.github.com>
2025-07-20 08:10:07 -0300

    feat: aula3.js module Check.

---
+++ class/aula3.js
@@ -0,0 +1,29 @@
+// Realizing Checks 
+    // Validam condicoes booleanas no teste de performance 
+        // Principalmente em pontos como: Se o sistema esta 
+        // respondendo com o conteudo que e esperado.
+
+import http from 'k6/http';
+
+// Nota1
+    // primeiro: importar o modulo do k6 que realiza esse tipo de validacao 
+    // https://grafana.com/docs/k6/latest/using-k6/modules/
+import check from 'k6';
+
+export const options = {
+    vus: 1,
+    duration: '3s'
+}
+
+export default function(){
+
+    // segundo: armazenar o conteudo que e retornado da requisicao 
+    // em algum lugar. Abaixo colocamos em uma contante.
+    const rest = http.get('http://test.k6.io');
+
+    // terceiro: utilizamos o modulo check para validar o objeto.
+    check(rest,{
+        'status coide é 200': (r) => r.status === 200
+        
+    })
+}
