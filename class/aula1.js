7567f66c61ab9cf35d44380b4d10be42292dcfb9
julianabaicar <baicarjuliana@gmail.com>
2025-07-20 08:36:47 -0300

    fix: ./class/aula1.js ajust comments in english.

--- class/aula1.js
+++ class/aula1.js
@@ -1,30 +1,28 @@
-// CICLO DE VIDA COM K6: bem definido, sempre passará por essas fases 
-// na mesma ordem
+// K6 LIFECYCLE: well defined, will always go through these phases 
+// in the same order
 
-
-// 1. inicializacao
-    // importacao de um modulo de sleep nativo do k6
+// 1. initialization
+    // import of the native sleep module from k6
 import sleep from 'k6';
 
-// 2. configuracao
-    // pode ter mais de um bloco de configuracao
+// 2. configuration
+    // can have more than one configuration block
 export const option = {
     vus: 1,
     duration: '10s'
 }
 
-// 3. execucao // codigo vu
+// 3. execution // vu code
 export default function(){
-    console.log("testando o k6");
+    console.log("testing k6");
     sleep(1);
 }
 
-
-// 4. desmontagem
-    // etapa opcional do teste
-    // util quando queremos enviar resultados para um webook, plataforma
-    // externa ou notificar;
-    // tambem responsavel por encerrar o teste
+// 4. teardown
+    // optional test step
+    // useful when we want to send results to a webhook, external platform
+    // or notify;
+    // also responsible for ending the test
 
 export function teardown(data){
     console.log(data);
